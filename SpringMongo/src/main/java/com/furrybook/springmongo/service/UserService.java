15bf47b7cc682e7f741b89f89e733161647b0e41
Jun Xian <junxian@Juns-MacBook-Pro.local>
2023-05-18 15:33:36 +0800

    db done

--- SpringMongo/src/main/java/com/furrybook/springmongo/service/UserService.java
+++ SpringMongo/src/main/java/com/furrybook/springmongo/service/UserService.java
@@ -1,13 +1,15 @@
 package com.furrybook.springmongo.service;
 
-import com.furrybook.springmongo.model.User.AdminUser;
-import com.furrybook.springmongo.model.User.StandardUser;
-import com.furrybook.springmongo.model.User.User;
-import com.furrybook.springmongo.repository.UserRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.furrybook.springmongo.model.StandardUser;
+import com.furrybook.springmongo.model.User.User;
+import com.furrybook.springmongo.repository.UserRepository;
+
+import main.java.com.furrybook.springmongo.model.User.AdminUser;
 
 @Service
 public class UserService {
