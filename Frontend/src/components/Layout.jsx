275bc29634762b4c6d0120eb740e07f99d61a867
Jun Xian <junxian@Juns-MacBook-Pro.local>
2023-05-20 00:19:51 +0800

    styled navbar

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,13 +1,33 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
-import { TempNavigate } from "./TempNavigate";
+import styled from 'styled-components';
+import { Navbar } from "./Navbar";
 import "./components.css"
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto;
+  height: 100vh;
+`;
+
+const LeftNavbar = styled(Navbar)`
+  grid-column: 1;
+  grid-row: 1;
+`;
+
+const Content = styled.div`
+  background-color: #F6F6F6;
+  grid-column: 2;
+  grid-row: 1;
+`;
 
 export function Layout(){
     return (
-        <div>
-            <TempNavigate />
-            <Outlet />
-        </div>
+        <Container>
+            <LeftNavbar/>
+            <Content>
+                <Outlet/>
+            </Content>
+        </Container>
     )
 }
