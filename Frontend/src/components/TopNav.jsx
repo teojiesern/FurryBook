3ee1172e5aefd0a095d100f1ff374f0f4a3ff1f7
teojiesern <teojiesern42@gmail.com>
2023-05-21 14:54:48 +0800

    done top nav bar, now to work on the functinoal connection part of the top nav bar

--- Frontend/src/components/TopNav.jsx
+++ Frontend/src/components/TopNav.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import { BiSearch } from "react-icons/bi";
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import { RiProfileFill } from "react-icons/ri";
+import { IoSettingsSharp, IoLogOut } from "react-icons/io5";
 
 const StyledDiv = styled.div`
     display: flex;
@@ -25,40 +25,107 @@ const StyledSearchBar = styled.input`
     }
 `;
 
-const StyledDropDown = styled(DropdownButton)`
-    width: 200px;
-    color: black;
+const StyledDropdown = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 30px;
+    right: 100px;
+`;
+
+const StyledPrimaryChoice = styled.div`
+    width: 180px;
+    height: 50px;
+    border-radius: 100px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    background-color: white;
+    display: flex;
+    cursor: pointer;
+    margin-left: auto;
+    margin-bottom: 5px;
+    transition: all 0.2s ease-out;
+
+    &:hover {
+        transform: scale(1.025);
+        transition: all 0.1s ease-out;
+    }
+`;
+
+const StyledProfilePic = styled.div`
+    border-radius: 100px;
+    width: 50px;
+    height: 50px;
+    background-image: url("/assets/profile pictures/IMG_1749.jpg");
+    background-size: cover;
+    background-position: center;
+    margin-right: 10px;
+`;
+
+const StyledUserInfoDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 50px;
+    justify-content: center;
+    align-self: flex-end;
+`;
+
+const StyledUserInfo = styled.h3`
+    font-size: 13px;
+    color: #153fac;
+    margin: 0;
+`;
+
+const StyledOption = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    background-color: white;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+    margin-bottom: 10px;
+    padding: 10px;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: 17px;
+`;
+
+const StyledOptionContainer = styled.div`
+    width: 300px;
+    background-color: white;
+    padding: 20px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
     border-radius: 10px;
-    border: none;
 `;
 
-const StyledOption = styled.option`
-    padding: 2rem;
-    background-color: #f2f2f2;
-    color: #333;
-    font-weight: bold;
-    border-bottom: 1px solid #ccc;
-    cursor: pointer;
-`;
-
-const StyledDropdownItem = styled(Dropdown.Item)`
-  background-color: #f2f2f2;
-  color: #333;
-
-  &:hover {
-    background-color: #ccc;
-    color: #fff;
-  }
+const StyledLeftPortion = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const searchIconStyle = {
     position: "absolute",
     top: "47px",
-    right: "44vw",
+    right: "38vw",
+};
+
+const marginRightStyle = {
+    marginRight: "10px",
 };
 
 export function TopNav() {
+    const [styling, setStyling] = React.useState("none");
+
+    function handleClick() {
+        setStyling((prevStyle) => (prevStyle === "none" ? "" : "none"));
+    }
+
+    function handleLogout() {
+        localStorage.removeItem("userId");
+        localStorage.removeItem("loggedIn");
+        window.location.reload();
+    }
+
     return (
         <div>
             <StyledDiv>
@@ -68,17 +135,38 @@ export function TopNav() {
                         <BiSearch style={searchIconStyle} />
                     </div>
                 </div>
-                <StyledDropDown
-                    title="Dropdown button"
-                >
-                    <StyledDropdownItem href="#/action-1">Action</StyledDropdownItem>
-                    <StyledDropdownItem href="#/action-2">
-                        Another action
-                    </StyledDropdownItem>
-                    <StyledDropdownItem href="#/action-3">
-                        Something else
-                    </StyledDropdownItem>
-                </StyledDropDown>
+                <StyledDropdown>
+                    <StyledPrimaryChoice onClick={handleClick}>
+                        <StyledProfilePic></StyledProfilePic>
+                        <StyledUserInfoDiv>
+                            <StyledUserInfo>Teo Jie Sern</StyledUserInfo>
+                            <StyledUserInfo>User</StyledUserInfo>
+                        </StyledUserInfoDiv>
+                    </StyledPrimaryChoice>
+                    <StyledOptionContainer style={{ display: styling }}>
+                        <StyledOption>
+                            <StyledLeftPortion>
+                                <RiProfileFill style={marginRightStyle} />
+                                Profile
+                            </StyledLeftPortion>
+                            &rarr;
+                        </StyledOption>
+                        <StyledOption>
+                            <StyledLeftPortion>
+                                <IoSettingsSharp style={marginRightStyle} />
+                                Settings
+                            </StyledLeftPortion>
+                            &rarr;
+                        </StyledOption>
+                        <StyledOption onClick={handleLogout}>
+                            <StyledLeftPortion>
+                                <IoLogOut style={marginRightStyle} />
+                                Logout
+                            </StyledLeftPortion>
+                            &rarr;
+                        </StyledOption>
+                    </StyledOptionContainer>
+                </StyledDropdown>
             </StyledDiv>
             <Outlet />
         </div>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
 export function Home() {
-    function handleClick() {
-        localStorage.removeItem("userId");
-        localStorage.removeItem("loggedIn");
-        window.location.reload();
-    }
 
     return (
-        <div>
-            <button onClick={handleClick}></button>
+        <div>hi
         </div>
     );
 }
