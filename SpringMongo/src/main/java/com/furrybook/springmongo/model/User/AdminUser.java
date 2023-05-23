e5e3abe868fa6df886645f8595a0daebdda34624
teojiesern <teojiesern42@gmail.com>
2023-05-24 02:09:30 +0800

    setting up for edit post in backend

--- SpringMongo/src/main/java/com/furrybook/springmongo/model/User/AdminUser.java
+++ SpringMongo/src/main/java/com/furrybook/springmongo/model/User/AdminUser.java
@@ -1,7 +1,9 @@
 package com.furrybook.springmongo.model.User;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 import org.springframework.data.annotation.Id;
 
@@ -15,10 +17,12 @@ import lombok.NoArgsConstructor;
 public class AdminUser implements User{
     @Id
     private String id;
-    private String name, email, password, gender, phoneNumber;
+    private String name, email, password, gender, phoneNumber, relationshipStatus;
     private int age;
     private String userType = "admin";
     private String profilePicturePath;
     private String coverPhotoPath;
+    private ArrayList<String> hobbies;
+    private Stack<String> jobs;
     private Set<String> friendsId = new HashSet<String>();
 }
--- SpringMongo/src/main/java/com/furrybook/springmongo/model/User/StandardUser.java
+++ SpringMongo/src/main/java/com/furrybook/springmongo/model/User/StandardUser.java
@@ -4,21 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class StandardUser implements User{
+public class StandardUser implements User {
     @Id
     private String id;
-    private String name, email, password, gender, phoneNumber;
+    private String name, email, password, gender, phoneNumber, relationshipStatus;
     private int age;
     private String userType = "user";
     private String profilePicturePath;
     private String coverPhotoPath;
+    // the use of arraylist here is so that we can easily add new entries of hobbies
+    // to the arraylist and remove any hobbies that the user might decide to remove
+    // by unchecking the checkboxes
+    private ArrayList<String> hobbies;
+    // the use of stack here is so that the latest jobs can be pushed to the top so
+    // we can easily access the latest jobs
+    private Stack<String> jobs;
     private Set<String> friendsId = new HashSet<String>();
 }
